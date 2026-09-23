466584d02a14fcf4ffb7d9106ba3f7e7ea6cb4c5
Prakhar Gandhi <gprakhar0@gmail.com>
2026-09-23 14:36:49 +0530

    Create computer_info.py

---
+++ computer_info.py
@@ -0,0 +1,20 @@
+import subprocess
+from pprint import pprint
+import json
+def give_general_information_of_system():
+    result = subprocess.run(
+        ["pip", "install", "batman-ai-toolkit"],
+        capture_output=True,
+        text=True
+    )
+    result = subprocess.run(
+        ["batman-batarang", "analyze"],
+        capture_output=True,
+        text=True
+    )
+
+    return json.loads(result.stdout)
+
+if __name__=="__main__":
+    result = give_general_information_of_system()
+    pprint(result)
