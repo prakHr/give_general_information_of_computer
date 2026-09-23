9c12d584d7e27da433a40ab6a007a5226e453cf0
Prakhar Gandhi <gprakhar0@gmail.com>
2026-09-23 14:46:11 +0530

    Update computer_info.py

--- computer_info.py
+++ computer_info.py
@@ -1,12 +1,14 @@
 import subprocess
 from pprint import pprint
 import json
-def give_general_information_of_system():
+def give_general_information_of_system(do_you_want_to_check_the_installation_is_proper):
     result = subprocess.run(
         ["pip", "install", "batman-ai-toolkit"],
         capture_output=True,
         text=True
     )
+    if do_you_want_to_check_the_installation_is_proper:
+        print(result)
     result = subprocess.run(
         ["batman-batarang", "analyze"],
         capture_output=True,
@@ -15,6 +17,7 @@ def give_general_information_of_system():
 
     return json.loads(result.stdout)
 
-# if __name__=="__main__":
-#     result = give_general_information_of_system()
-#     pprint(result)
+if __name__=="__main__":
+    do_you_want_to_check_the_installation_is_proper = True
+    result = give_general_information_of_system(do_you_want_to_check_the_installation_is_proper)
+    pprint(result)
